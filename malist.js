32d7c1b317a7396c91a63cffc860f38391b28a8f
aniritha68 <54759465+aniritha68@users.noreply.github.com>
2021-01-18 17:45:55 +0530

    Create malist.js

---
+++ malist.js
@@ -0,0 +1,15 @@
+$(document.body).click(function(){
+$( this ).id;
+ if(id==b1){
+  printVal(list1)
+}else  if(id==b2){
+ printVal(list2)
+}
+
+ 
+});
+
+printVal(input){
+ loop over the list based on above input
+
+}
